b2265d721e2c04eb3b2313f1b62f8e5eb36b9da8
Oliver Denton <odenton@criticalsoftware.com>
2020-10-14 17:10:39 +0100

    added input box, and diplay of previous inputs

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import TodoForm from '../TodoForm';
 
 export default class TodoList extends Component {
+  state = {
+    todos: [],
+  };
+
   render() {
-    return <div>todos...</div>;
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {this.state.todos.map((todo) => (
+          <div key={todo.id}> {todo.text} </div>
+        ))}
+      </div>
+    );
   }
+
+  addTodo = (todo) => {
+    //dont wanna mutate the state, so want to create a copy of our empty array, add an item to the start, and reassign
+    this.setState({ todos: [todo, ...this.state.todos] });
+  };
 }
